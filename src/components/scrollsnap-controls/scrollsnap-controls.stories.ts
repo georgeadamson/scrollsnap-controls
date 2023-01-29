373f203109d461334867d43f63db25123057e415
George <george@yotoplay.com>
2023-01-29 15:10:17 +0000

    Feature: More examples in story

--- src/components/scrollsnap-controls/scrollsnap-controls.stories.ts
+++ src/components/scrollsnap-controls/scrollsnap-controls.stories.ts
@@ -1,14 +1,21 @@
 // import { Story, Meta } from '@storybook/react'
 
+const slideMarkup = `
+<li class="app-slide">
+  <h3 class="app-slide-heading">Make Your Own</h3>
+  <p class="app-slide-text">Where the fun happens! Record stories in your own voice, make playlists from your MP3s.</p>
+  <img class="app-slide-img" src="https://source.unsplash.com/random/50x50" />
+</li>
+`
+
 export default {
     // this creates a ‘Components’ folder and a ‘MyComponent’ subfolder
     title: './scrollsnap-controls.tsx',
 } ;
 
-const Template = (args) => `
+const Template = ({notify}) => `
 <style>
-
-.app-slider {
+  .app-slider {
     padding: 0;
     width: 100%;
     overflow-x: auto;
@@ -23,51 +30,44 @@ const Template = (args) => `
 
   .app-slider > * {
     scroll-snap-align: center;
-    scroll-snap-stop: always;
-    min-width: 50vw;
+    xscroll-snap-stop: always;
+    min-width: 150px;
   }
 </style>
 
+<div style="width:300px">
+  <ul id="demo-slider1" class="app-slider">
+    ${ slideMarkup.repeat(10) }
+  </ul>
 
-<ul id="demo-slider" class="app-slider">
-<li class="app-slide">
-  <h3 class="app-slide-heading">Make Your Own</h3>
-  <p class="app-slide-text">Where the fun happens! Record stories in your own voice, make playlists from your MP3s, and link them all to physical cards</p>
-  <h4 class="app-slide-label">Podcasts</h4>
-  <img class="app-slide-img" src="https://source.unsplash.com/random/40x50?1" />
-</li>
+  <button id="prev1">Prev</button>
+  <button id="next1">Next</button>
+  <scrollsnap-controls ${notify} prev="#prev1" next="#next1"></scrollsnap-controls>
+</div>
 
-<li class="app-slide">
-  <h3 class="app-slide-heading">Make Your Own</h3>
-  <p class="app-slide-text">Where the fun happens! Record stories in your own voice, make playlists from your MP3s, and link them all to physical cards</p>
-  <h4 class="app-slide-label">Podcasts</h4>
-  <img class="app-slide-img" src="https://source.unsplash.com/random/40x50?2" />
-</li>
+<div style="width:400px">
+  <ul id="demo-slider2" class="app-slider">
+    ${ slideMarkup.repeat(10) }
+  </ul>
 
-<li class="app-slide">
-  <h3 class="app-slide-heading">Make Your Own</h3>
-  <p class="app-slide-text">Where the fun happens! Record stories in your own voice, make playlists from your MP3s, and link them all to physical cards</p>
-  <h4 class="app-slide-label">Podcasts</h4>
-  <img class="app-slide-img" src="https://source.unsplash.com/random/40x50?3" />
-</li>
+  <button id="prev2">Prev</button>
+  <button id="next2">Next</button>
+  <scrollsnap-controls ${notify} prev="#prev2" next="#next2"></scrollsnap-controls>
+</div>
 
-<li class="app-slide">
-  <h3 class="app-slide-heading">Make Your Own</h3>
-  <p class="app-slide-text">Where the fun happens! Record stories in your own voice, make playlists from your MP3s, and link them all to physical cards</p>
-  <h4 class="app-slide-label">Podcasts</h4>
-  <img class="app-slide-img" src="https://source.unsplash.com/random/40x50?4" />
-</li>
-</ul>
+<div style="width:700px">
+  <ul id="demo-slider3" class="app-slider">
+    ${ slideMarkup.repeat(10) }
+  </ul>
 
-<scrollsnap-controls first="${args.first}" middle="${args.middle}" last="${args.last}" notify prev=".prev" next=".next"></scrollsnap-controls>
-<button class="prev">Prev</button>
-<button class="next">Next</button>
+  <button id="prev3">Prev</button>
+  <button id="next3">Next</button>
+  <scrollsnap-controls ${notify} prev="#prev3" next="#next3"></scrollsnap-controls>
+</div>
 `;
 
 export const Example = Template.bind({});
 
 Example.args = {
-  first: 'Winnie',
-  middle: 'The',
-  last: 'Pooh'
+  notify: false,
 };
